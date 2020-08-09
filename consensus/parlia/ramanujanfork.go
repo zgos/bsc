995f7411365047a5c1333a88f7e4b22e8025b237
fudongbai <296179868@qq.com>
2020-08-09 11:56:12 +0800

    abcd

--- consensus/parlia/ramanujanfork.go
+++ consensus/parlia/ramanujanfork.go
@@ -27,7 +27,7 @@ func (p *Parlia) delayForRamanujanFork(snap *Snapshot, header *types.Header) tim
 func (p *Parlia) blockTimeForRamanujanFork(snap *Snapshot, header, parent *types.Header) uint64 {
 	blockTime := parent.Time + p.config.Period
 	if p.chainConfig.IsRamanujan(header.Number) {
-		log.Info("==debug prepare blockTimeForRamanujanFork", "headerTime", header.Time, "height", "diff", header.Difficulty, "backoff", backOffTime(snap, p.val))
+		log.Info("=== debug prepare blockTimeForRamanujanFork", "headerTime", header.Time, "height", "diff", header.Difficulty, "backoff", backOffTime(snap, p.val))
 		blockTime = blockTime + backOffTime(snap, p.val)
 	}
 	return blockTime
