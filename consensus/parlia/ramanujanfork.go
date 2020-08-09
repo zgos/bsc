2962e7627d775711a38a19aef18aafbe66cd067c
fudongbai <296179868@qq.com>
2020-08-09 12:06:42 +0800

    abcd

--- consensus/parlia/ramanujanfork.go
+++ consensus/parlia/ramanujanfork.go
@@ -18,7 +18,10 @@ const (
 func (p *Parlia) delayForRamanujanFork(snap *Snapshot, header *types.Header) time.Duration {
 	delay := time.Unix(int64(header.Time), 0).Sub(time.Now()) // nolint: gosimple
 	if p.chainConfig.IsRamanujan(header.Number) {
+		log.Info("=== debug delayForRamanujanFork")
 		return delay
+	} else {
+		log.Info("=== debug not delayForRamanujanFork")
 	}
 	wiggle := time.Duration(len(snap.Validators)/2+1) * wiggleTimeBeforeFork
 	return delay + time.Duration(fixedBackOffTimeBeforeFork) + time.Duration(rand.Int63n(int64(wiggle)))
@@ -29,6 +32,8 @@ func (p *Parlia) blockTimeForRamanujanFork(snap *Snapshot, header, parent *types
 	if p.chainConfig.IsRamanujan(header.Number) {
 		log.Info("=== debug prepare blockTimeForRamanujanFork", "headerTime", header.Time, "height", "diff", header.Difficulty, "backoff", backOffTime(snap, p.val))
 		blockTime = blockTime + backOffTime(snap, p.val)
+	} else {
+		log.Info("=== debug prepare not blockTimeForRamanujanFork")
 	}
 	return blockTime
 }
@@ -38,6 +43,8 @@ func (p *Parlia) blockTimeVerifyForRamanujanFork(snap *Snapshot, header, parent 
 		if header.Time < parent.Time+p.config.Period+backOffTime(snap, header.Coinbase) {
 			return consensus.ErrFutureBlock
 		}
+	} else {
+		log.Info("=== debug not blockTimeVerifyForRamanujanFork")
 	}
 	return nil
 }
